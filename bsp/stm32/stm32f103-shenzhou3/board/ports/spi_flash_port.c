0fc7da257f95a3e7c5bf6132399fa7f06a055bc2
farewellbuaa <383967403@qq.com>
2020-07-07 19:19:59 +0800

    配置dfs，可以访问flash上面的fat文件系统了

--- bsp/stm32/stm32f103-shenzhou3/board/ports/spi_flash_port.c
+++ bsp/stm32/stm32f103-shenzhou3/board/ports/spi_flash_port.c
@@ -2,6 +2,9 @@
 
 #ifdef BSP_USING_SPI_FLASH
 #include <drv_spi.h>
+#include <spi_flash.h>
+#include <spi_flash_sfud.h>
+#include <dfs_fs.h>
 
 int spi_flash_init(void)
 {
@@ -11,4 +14,26 @@ int spi_flash_init(void)
 	
 }
 INIT_DEVICE_EXPORT(spi_flash_init);
+
+int spi_flash_mount(void)
+{
+	static rt_spi_flash_device_t rtt_dev = NULL;
+	rtt_dev = rt_sfud_flash_probe("flash0", "spi10");
+	
+	if (rtt_dev==NULL)
+		return RT_ERROR;
+	
+	if (dfs_mount("flash0", "/", "elm", 0, 0) == RT_EOK)
+	{
+		rt_kprintf("flash0 mount to '/'\n");
+		return RT_EOK;
+	}
+	else
+	{
+		rt_kprintf("flash0 mount to '/' failed!\n");
+		return RT_ERROR;
+	}
+	
+}
+INIT_APP_EXPORT(spi_flash_mount);
 #endif /*BSP_USING_SPI_FLASH*/
--- bsp/stm32/stm32f103-shenzhou3/rtconfig.h
+++ bsp/stm32/stm32f103-shenzhou3/rtconfig.h
@@ -79,6 +79,18 @@
 #define DFS_FILESYSTEMS_MAX 2
 #define DFS_FILESYSTEM_TYPES_MAX 2
 #define DFS_FD_MAX 16
+#define RT_USING_DFS_ELMFAT
+
+/* elm-chan's FatFs, Generic FAT Filesystem Module */
+
+#define RT_DFS_ELM_CODE_PAGE 437
+#define RT_DFS_ELM_WORD_ACCESS
+#define RT_DFS_ELM_USE_LFN_3
+#define RT_DFS_ELM_USE_LFN 3
+#define RT_DFS_ELM_MAX_LFN 255
+#define RT_DFS_ELM_DRIVES 2
+#define RT_DFS_ELM_MAX_SECTOR_SIZE 4096
+#define RT_DFS_ELM_REENTRANT
 #define RT_USING_DFS_DEVFS
 
 /* Device Drivers */
@@ -89,6 +101,11 @@
 #define RT_SERIAL_USING_DMA
 #define RT_SERIAL_RB_BUFSZ 64
 #define RT_USING_PIN
+#define RT_USING_SPI
+#define RT_USING_SFUD
+#define RT_SFUD_USING_SFDP
+#define RT_SFUD_USING_FLASH_INFO_TABLE
+#define RT_SFUD_SPI_MAX_HZ 50000000
 
 /* Using USB */
 
@@ -151,6 +168,7 @@
 
 /* peripheral libraries and drivers */
 
+#define BSP_USING_SPI1
 
 /* miscellaneous packages */
 
@@ -166,11 +184,14 @@
 
 /* Onboard Peripheral Drivers */
 
+#define BSP_USING_SPI_FLASH
+
 /* On-chip Peripheral Drivers */
 
 #define BSP_USING_GPIO
 #define BSP_USING_UART
 #define BSP_USING_UART1
+#define BSP_USING_SPI
 
 /* Board extended module Drivers */
 
